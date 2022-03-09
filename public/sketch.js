5d6a62e8a7d6ff3ee9e96d072b57d64e65ad7183
Reginald Robinson <ddrbnsn@gmail.com>
2022-03-08 20:12:05 -0800

    removed nm from repo

--- public/sketch.js
+++ public/sketch.js
@@ -8,7 +8,7 @@ let size = 20, color
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(20);
+  background(0);
   noStroke()
   color = {
     r: random(255),
@@ -16,14 +16,14 @@ function setup() {
     b: random(255)
   }
 
-  socket.on('broadcastDrawing', (data) => {
-    fill(color.r, color.g, color.b)
-    ellipse(data.x, data.y, size, size)
-  })
+  // socket.on('broadcastDrawing', (data) => {
+  //   fill(color.r, color.g, color.b)
+  //   ellipse(data.x, data.y, size, size)
+  // })
 }
 
 function displayClients(clients) {
- console.log
+//  console.log
 }
 
 
@@ -32,16 +32,25 @@ function keyPressed() {
     color.r = random(255)
     color.g = random(255)
     color.b = random(255)
+    console.log("hey")
+  }
+  if(key == 'b'){
+    color.r = 0
+    color.g = 0
+    color.b = 0
+  }
+  if(key =='r'){
+    background(0)
   }
 }
 
 function mouseDragged() {
-  fill(255)
+  fill(color.r,color.g,color.b)
   ellipse(mouseX, mouseY, size)
   let data = {
     x: mouseX,
     y: mouseY,
     c: color
   }
-  socket.emit('clientDrawing', data)
+  // socket.emit('clientDrawing', data)
 }
